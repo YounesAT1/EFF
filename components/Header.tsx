0b391766b5576d7d90a99153d56d72dc2f17ca3d
YounesAT1 <youness.monkyde@gmail.com>
2023-11-06 19:23:44 +0100

    adding header

---
+++ components/Header.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import Link from "next/link";
+import { useTheme } from "next-themes";
+import { Sun, Moon, Menu } from "lucide-react";
+import ProfileButton from "./ui/ProfileButton";
+import Container from "./ui/Container";
+import { Button } from "./ui/Button";
+import { Sheet, SheetContent, SheetTrigger } from "./ui/sheet";
+import Copyright from "./CopyRight";
+
+const routes = [
+  {
+    href: "/flights",
+    label: "Flights",
+  },
+  {
+    href: "/hotels",
+    label: "Hotels",
+  },
+  {
+    href: "/taxis",
+    label: "Taxis",
+  },
+  {
+    href: "/about",
+    label: "About",
+  },
+  {
+    href: "/services",
+    label: "Services",
+  },
+];
+
+const Header = () => {
+  const { theme, setTheme } = useTheme();
+
+  return (
+    <header className="sm:flex sm:justify-between py-3 px-4 border-b">
+      <Container>
+        <div className="relative px-4 sm:px-6 lg:px-8 flex h-16 items-center justify-between">
+          <div className="flex items-center">
+            <Link href="/" className="ml-4 lg:ml-0">
+              <h1 className="text-xl font-bold text-purple-700 ">Travely</h1>
+            </Link>
+          </div>
+          <nav className="mx-6 flex items-center space-x-4 lg:space-x-6 hidden md:block">
+            {routes.map((route, index) => (
+              <Button asChild variant="ghost" key={index}>
+                <Link
+                  href={route.href}
+                  className="text-sm font-medium transition-colors"
+                >
+                  {route.label}
+                </Link>
+              </Button>
+            ))}
+          </nav>
+          <div className="flex items-center">
+            <ProfileButton />
+            <Button
+              variant="ghost"
+              size="icon"
+              aria-label="Toggle Theme"
+              className="mr-6"
+              onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
+            >
+              <Sun className="h-6 w-6 rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+              <Moon className="absolute h-6 w-6 rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
+            </Button>
+            <Sheet>
+              <SheetTrigger>
+                <Menu className="h-6 md:hidden" />
+              </SheetTrigger>
+              <SheetContent side="right" className="w-[300px] sm:w-[400px]">
+                <div className="flex flex-col justify-between h-full">
+                  <nav className="flex flex-col gap-4">
+                    {routes.map((route, index) => (
+                      <>
+                        <Link
+                          key={index}
+                          href={route.href}
+                          className="block px-4 py-2 text-lg 
+                          "
+                        >
+                          {route.label}
+                        </Link>
+                      </>
+                    ))}
+                  </nav>
+                  <div className="flex items-center justify-center">
+                    <Copyright />
+                  </div>
+                </div>
+              </SheetContent>
+            </Sheet>
+          </div>
+        </div>
+      </Container>
+    </header>
+  );
+};
+
+export default Header;
---
+++ components/ui/ProfileButton.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { useState } from "react";
+import { Button } from "./Button";
+import { User, LogOut } from "lucide-react";
+import Link from "next/link";
+
+const ProfileButton = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
+  return (
+    <div className="mr-6">
+      {isLoggedIn ? (
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <Avatar>
+              <AvatarImage src="/favicon.ico" />
+              <AvatarFallback>AT</AvatarFallback>
+            </Avatar>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent>
+            <DropdownMenuLabel>My Account</DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem className="cursor-pointer ">
+              <Link href="/profile" className="flex  items-center gap-2">
+                <User strokeWidth={1} />
+                <h1 className="text-l ">Profile</h1>
+              </Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem className="cursor-pointer">
+              <Link href="/profile/logout" className="flex  items-center gap-2">
+                <LogOut strokeWidth={1} /> <h1 className="text-l ">Log out</h1>
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      ) : (
+        <Button
+          className="flex items-center justify-between gap-2 mr-6"
+          variant="default"
+        >
+          <User />
+          <h1>Log in</h1>
+        </Button>
+      )}
+    </div>
+  );
+};
+
+export default ProfileButton;
